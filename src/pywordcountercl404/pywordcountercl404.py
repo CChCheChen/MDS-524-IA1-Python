e4502ba606bfabe15274091de36d57540c1e7329
Chen Lin <chen.lin.0404@gmail.com>
2023-01-11 02:47:53 -0800

    update readme file

--- src/pywordcountercl404/pywordcountercl404.py
+++ src/pywordcountercl404/pywordcountercl404.py
@@ -3,7 +3,14 @@ from string import punctuation
 
 
 def load_text(input_file):
-    """Load text from a text file and return as a string."""
+    """_summary_
+
+    Args:
+        input_file (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     with open(input_file, "r") as file:
         text = file.read()
     return text
